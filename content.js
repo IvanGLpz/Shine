081cbc81c446557e26316cee220ef50a225d46a2
Milton Ivan (WhiL) García López <67771688+Whil117@users.noreply.github.com>
2023-08-21 12:38:39 -0600

    update header with elements

--- content.js
+++ content.js
@@ -9,6 +9,13 @@ const iconTranslateSVG = `
 </svg>
 
 `
+const iconTranslateBlackSVG = ` 
+
+<svg width="30" height="30" viewBox="0 0 57 51" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M28.1131 51L39.8484 20.4H45.2647L57 51H51.5837L48.8756 43.2225H36.3665L33.5294 51H28.1131ZM37.914 38.76H47.1991L42.6855 26.1375H42.4276L37.914 38.76ZM7.73756 43.35L4.1267 39.78L17.1516 26.9025C15.5181 25.1175 14.0884 23.2688 12.8624 21.3563C11.6364 19.4438 10.5729 17.425 9.67195 15.3H15.0882C15.862 16.83 16.6899 18.2112 17.572 19.4437C18.4541 20.6762 19.5176 21.9725 20.7624 23.3325C22.6538 21.2925 24.2229 19.199 25.4695 17.0519C26.7161 14.9048 27.7692 12.6208 28.629 10.2H0V5.1H18.0543V0H23.2127V5.1H41.267V10.2H33.7873C32.8846 13.2175 31.6595 16.15 30.112 18.9975C28.5645 21.845 26.6516 24.5225 24.3733 27.03L30.5633 33.2775L28.629 38.505L20.6335 30.6L7.73756 43.35Z" fill="#000000"/>
+</svg>
+
+`
 
 const toolbar = document.createElement('div')
 const selectLanguage = document.createElement('select')
@@ -56,9 +63,11 @@ toolbar.style.gridTemplateColumns = "1fr 1fr 1fr";
 
 ///TITLE
 
-titleNode.textContent = "Shine"
+titleNode.innerHTML = `${iconTranslateBlackSVG} Shine`
 titleNode.style.color = "#000000"
 titleNode.style.fontWeight = "700"
+titleNode.style.display = "flex"
+titleNode.style.alignItems = "center"
 ///CONTAINER
 
 
